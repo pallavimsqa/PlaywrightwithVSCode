9a3b96582c91a74267638e9be9cce2d7e5bb027d
pallavimsqa <pallavimshsn@gmail.com>
2026-03-03 13:39:45 -0400

    chore: save workspace changes

--- Tasks_practice/For_Loop_task1.js
+++ Tasks_practice/For_Loop_task1.js
@@ -1,11 +1,45 @@
 // For Loop
-
 //Print "Hello" 5 times
+/*for(let i=0; i<5 ; i++){
+    console.log("Hello");
+}*/
+
 
 // Print numbers from 1 to 10
+/*for(let i=1; i<=10 ; i++){
+    console.log(i);
+}*/
+
 
 // Print even numbers from 1 to 20
+/*for(let i=1; i<=20 ; i++){
+
+    if(i%2 ===0 ){
+        console.log(i);
+    }
+    
+}*/
+
+
 
 //Print the sum of first 10 natural numbers
+/*let sum = 0;
+
+for(let i=1; i<=10 ; i++){
+
+    sum += i;
+   
+    }
+     console.log("Sum of first 10 nutural numbers is " , sum);*/
+
 
 //Print the multiplication table of 5
+
+for(let i=5; i<=50 ; i++){
+
+    if (i% 5 === 0){
+        console.log(i);
+    }
+ 
+    }
+     
--- Tasks_practice/Leap_Year_Checker.js
+++ Tasks_practice/Leap_Year_Checker.js
@@ -3,3 +3,20 @@
 //A leap year is divisible by 4, but not by 100 unless it is also divisible by 400. 
 // Use an if-else statement to make this determination.
 //year = 2026 , no
+
+let year = 2026;
+
+// edge caese to check the given number
+if(typeof year !== "number" || year <= 0){
+    console.log(`Given year ${year} is not a valid year`);
+    return;
+}
+
+if ( year % 4 === 0 || (year% 100 === 0 && year % 400 ===0))
+    {
+        console.log(`Given year ${year} is a leep year`);
+
+    }
+    else{
+        console.log(`Given year ${year} is not a leep year`);
+    }
